842d4d2cad5d61363fc388b89f7b57f9ba1f3d59
RongYu98 <Stuy.rong.yu@gmail.com>
2016-02-29 19:21:35 -0500

    work

--- hw04/svg.js
+++ hw04/svg.js
@@ -1,5 +1,8 @@
 var pic = document.getElementById("vimage");
 
+
+console.log("HI!");
+
 /*
 var c = document.createElementNS( "http://www.w3.org/2000/svg", "circle" );
 
@@ -13,30 +16,30 @@ console.log("Gu");
 pic.appendChild(c);
 
 */
-console.log("Hi");
 
-var change(e){
+var change = function(e){
     e.preventDefault();
     this.setAttribute("fill","green");
 };
 
+
 var drawDot = function( x,y ){
-
     var c = document.createElementNS( "http://www.w3.org/2000/svg", "circle" );
-//mess up the url, it doesn't work at ALL
-
-    c.setAttribute("cx", 0);
-    c.setAttribute("cy", 10);
+    c.setAttribute("cx", x);
+    c.setAttribute("cy", y);
     c.setAttribute( "r", 100); //Quotes around numbers do not matter
     c.setAttribute( "fill", "red");
     c.setAttribute( "stroke", "black" );
-    console.log("Gu");
+    //console.log("Gu");
     pic.appendChild(c);
-}
+
+};
 
 var clicked =  function(e){
     if (e.toElement == this){
 	drawDot( e.offsetX, e.offsetY );
+        console.log("Hi");
     }
 };
+
 pic.addEventListener("click", clicked);
