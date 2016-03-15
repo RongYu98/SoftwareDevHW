6898f996542f36b6d1948fb9cf3d5b39c9adf372
RongYu98 <Stuy.rong.yu@gmail.com>
2016-03-15 17:47:50 -0400

    the js won't even run, link is broken

--- LoremIpsum/d3js.js
+++ LoremIpsum/d3js.js
@@ -1,5 +1,7 @@
 var paragraphs = document.getElementsByTagName("p");
+console.log("Hi");
 for (var i = 0; i < paragraphs.length; i++) {
   var paragraph = paragraphs.item(i);
   paragraph.style.setProperty("color", "white", null);
+  conosle.log("HI");
 }
