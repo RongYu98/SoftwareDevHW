cba911c574b792fd02aed74a34b4e7820482fba1
RongYu <stuy.rong.yu@gmail.com>
2016-03-16 11:32:42 -0400

    work

--- LoremIpsum/d3js.js
+++ LoremIpsum/d3js.js
@@ -1,5 +1,7 @@
 var paragraphs = document.getElementsByTagName("p");
 for (var i = 0; i < paragraphs.length; i++) {
   var paragraph = paragraphs.item(i);
-  paragraph.style.setProperty("color", "white", null);
+  paragraph.style.setProperty("color", "red", null);
 }
+d3.selectAll("p").style("color", "white");
+console.log("hi");
